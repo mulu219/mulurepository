c967ad864e8cdeb68947d34ec341620f29034b0f
mulu219 <malone.kidane@gmail.com>
2022-09-28 21:07:39 -0400

    mgr pg, auth,

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://malonekidane:Opportuniti21!@mulu.x6htzeg.mongodb.net/test?retryWrites=true&w=majority", 
+    'dbName': 'demo'
+};
+
